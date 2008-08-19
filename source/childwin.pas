Echnaton70/HeidiSQL

commit 64f84fdfa235e0d6e543924023581f263bc43fe4

Add some more support for databases and tables with unicode characters.
git-svn-id: http://heidisql.googlecode.com/svn/trunk@1635 560a3889-bb33-0410-9f33-43a8a102dc09

source/fieldeditor.pas
@@ -11,7 +11,7 @@ interface
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   StdCtrls, ComCtrls, ImgList, ToolWin, ExtCtrls, Buttons, VirtualTrees,
-  PngSpeedButton;
+  PngSpeedButton, TntStdCtrls;
 
 type
   TFieldEditorMode = (femFieldAdd,femFieldUpdate,femIndexEditor);
@@ -42,8 +42,8 @@ type
     ButtonAdd: TButton;
     ButtonDelete: TButton;
     lblColumnsUsed: TLabel;
-    listColumnsUsed: TListBox;
-    listColumnsAvailable: TListBox;
+    listColumnsUsed: TTNTListBox;
+    listColumnsAvailable: TTNTListBox;
     btnAddColumnToIndex: TPngSpeedButton;
     btnDeleteColumnFromIndex: TPngSpeedButton;
     lblColumnsAvailable: TLabel;
@@ -289,7 +289,7 @@ begin
   ds.Close;
   FreeAndNil(ds);
 
-  listColumnsAvailable.Items := GetVTCaptions(cwin.ListColumns);
+  listColumnsAvailable.Items.Text := GetVTCaptions(cwin.ListColumns).Text;
   showkeys();
 end;
 
@@ -626,7 +626,7 @@ procedure TFieldEditForm.ComboBoxKeysChange(Sender: TObject);
 var i, j : Integer;
 begin
   if ComboBoxKeys.ItemIndex > -1 then begin
-    listColumnsAvailable.Items := GetVTCaptions(Mainform.ChildWin.ListColumns);
+    listColumnsAvailable.Items.Text := GetVTCaptions(Mainform.ChildWin.ListColumns).Text;
     for i:=0 to klist[ComboBoxKeys.ItemIndex].columns.Count-1 do
     begin
       j := listColumnsAvailable.Items.IndexOf(klist[ComboBoxKeys.ItemIndex].columns[i]);
@@ -634,7 +634,7 @@ begin
         listColumnsAvailable.Items.Delete( j );
     end;
     with klist[ComboBoxKeys.ItemIndex] do begin
-      listColumnsUsed.Items := Columns;
+      listColumnsUsed.Items.Text := Columns.Text;
       CheckBoxUnique.OnClick := nil;
       CheckBoxUnique.Checked := Unique;
       CheckBoxUnique.OnClick := CheckBoxUniqueClick;
@@ -962,7 +962,7 @@ end;
 procedure TFieldEditForm.btnAddAllColumnsToIndexClick(Sender: TObject);
 begin
   listColumnsUsed.Items.AddStrings(listColumnsAvailable.Items);
-  klist[ComboBoxKeys.ItemIndex].Columns.AddStrings(listColumnsAvailable.Items);
+  klist[ComboBoxKeys.ItemIndex].Columns.Append(listColumnsAvailable.Items.Text);
   listColumnsAvailable.Items.Clear;
   klist[ComboBoxKeys.ItemIndex].Modified := true;
   ValidateControls;

source/childwin.dfm
@@ -758,7 +758,7 @@ object MDIChild: TMDIChild
           BevelOuter = bvNone
           BorderWidth = 1
           TabOrder = 0
-          object lblDataTop: TLabel
+          object lblDataTop: TTntLabel
             Left = 1
             Top = 1
             Width = 287

source/column_selection.dfm
@@ -55,7 +55,7 @@ object ColumnSelectionForm: TColumnSelectionForm
       TabOrder = 1
       OnClick = chkSelectAllClick
     end
-    object chklistColumns: TCheckListBox
+    object chklistColumns: TTntCheckListBox
       Left = 4
       Top = 4
       Width = 158

source/column_selection.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, CheckLst, ExtCtrls, Registry;
+  Dialogs, StdCtrls, CheckLst, ExtCtrls, Registry, TntCheckLst, WideStrings;
 
 type
   TColumnSelectionForm = class(TForm)
@@ -12,7 +12,7 @@ type
     btnCancel: TButton;
     btnOK: TButton;
     chkSelectAll: TCheckBox;
-    chklistColumns: TCheckListBox;
+    chklistColumns: TTNTCheckListBox;
     chkSort: TCheckBox;
     procedure btnCancelClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
@@ -44,10 +44,10 @@ uses MAIN, helpers;
 }
 procedure TColumnSelectionForm.FormShow(Sender: TObject);
 var
-  reg_columns : TStringList;
+  reg_columns : TWideStringList;
 begin
   // Take column names from listColumns and add here
-  chklistColumns.Items := GetVTCaptions(Mainform.Childwin.ListColumns);
+  chklistColumns.Items.Text := GetVTCaptions(Mainform.Childwin.ListColumns).Text;
 
   // Set global reg_name (also used in btnOKClick)
   reg_name := REGNAME_DISPLAYEDCOLUMNS + '_' + Mainform.Childwin.ActiveDatabase + '.' + Mainform.Childwin.SelectedTable;
@@ -199,7 +199,7 @@ begin
 
     // Add all fieldnames again and check those which are in the checkedfields list
     chklistColumns.Items.BeginUpdate;
-    chklistColumns.Items := GetVTCaptions(Mainform.Childwin.ListColumns);
+    chklistColumns.Items.Text := GetVTCaptions(Mainform.Childwin.ListColumns).Text;
     for i := 0 to chklistColumns.Items.Count-1 do
     begin
       if checkedfields.IndexOf( chklistColumns.Items[i] ) > -1 then

source/copytable.pas
@@ -10,7 +10,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, Buttons, CheckLst, ZDataSet, ComCtrls, Registry;
+  StdCtrls, Buttons, CheckLst, ZDataSet, ComCtrls, Registry, WideStrings;
 
 type
   TCopyTableForm = class(TForm)
@@ -203,8 +203,8 @@ begin
       begin
         setlength(keylist, length(keylist)+1);
         which := high(keylist);
-        keylist[which].Columns := TStringList.Create;
-        keylist[which].SubParts := TStringList.Create;
+        keylist[which].Columns := TWideStringList.Create;
+        keylist[which].SubParts := TWideStringList.Create;
         with keylist[which] do // set properties for new key
         begin
           if Mainform.ChildWin.mysql_version < 40002 then

source/createdatabase.dfm
@@ -54,7 +54,7 @@ object CreateDatabaseForm: TCreateDatabaseForm
     Anchors = [akLeft, akRight, akBottom]
     Caption = 'SQL &preview for CREATE DATABASE:'
   end
-  object editDBName: TEdit
+  object editDBName: TTntEdit
     Left = 88
     Top = 16
     Width = 221

source/createdatabase.pas
@@ -4,11 +4,11 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, db, Registry, SynEdit, SynMemo;
+  Dialogs, StdCtrls, db, Registry, SynEdit, SynMemo, TntStdCtrls;
 
 type
   TCreateDatabaseForm = class(TForm)
-    editDBName: TEdit;
+    editDBName: TTNTEdit;
     lblDBName: TLabel;
     comboCharset: TComboBox;
     lblCharset: TLabel;
@@ -26,7 +26,7 @@ type
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
-    function GetCreateStatement: String;
+    function GetCreateStatement: WideString;
   private
     { Private declarations }
     dsCollations : TDataSet;
@@ -34,7 +34,7 @@ type
     currentCollation : String;
   public
     { Public declarations }
-    modifyDB : String;
+    modifyDB : WideString;
   end;
 
 
@@ -100,7 +100,7 @@ procedure TCreateDatabaseForm.FormShow(Sender: TObject);
 var
   selectCharset,
   currentCharset,
-  sql_create : String;
+  sql_create : WideString;
 begin
   if modifyDB = '' then
   begin
@@ -224,7 +224,7 @@ end;
 }
 procedure TCreateDatabaseForm.btnOKClick(Sender: TObject);
 var
-  sql : String;
+  sql : WideString;
   cwin: TMDIChild;
 begin
   cwin := Mainform.Childwin;
@@ -281,7 +281,7 @@ end;
 {**
   Generate CREATE DATABASE statement, used for preview and execution
 }
-function TCreateDatabaseForm.GetCreateStatement: String;
+function TCreateDatabaseForm.GetCreateStatement: WideString;
 begin
   Result := 'CREATE DATABASE ' + Mainform.Childwin.mask( editDBName.Text );
   if comboCharset.Enabled and (comboCharset.Text <> '') then

commit 02a18d7985527336e9ce0491a2d977723ebe804b

Fix bug #679 and friends: Database name has a new line attached to it. Broken in r1635. TWideStrings.Append doesn't explode its text parameter to multiple items, it's meant to append one single item.
git-svn-id: http://heidisql.googlecode.com/svn/trunk@1685 560a3889-bb33-0410-9f33-43a8a102dc09

source/childwin.pas
@@ -2659,8 +2659,10 @@ begin
   if (SynCompletionProposal1.ItemList.count = 0) and (Length(CurrentInput)>0) then
   begin
     // Add databases
-    SynCompletionProposal1.InsertList.Append( Databases.Text );
-    SynCompletionProposal1.ItemList.Append( Databases.Text );
+    for i := 0 to Databases.Count - 1 do begin
+      SynCompletionProposal1.InsertList.Add(Databases[i]);
+      SynCompletionProposal1.ItemList.Add(Databases[i]);
+    end;
     for i:=0 to SynCompletionProposal1.ItemList.count-1 do
       SynCompletionProposal1.ItemList[i] := '\hspace{2}\color{'+ColorToString(SynSQLSyn1.TableNameAttri.Foreground)+'}database\color{clWindowText}\column{}' + SynCompletionProposal1.ItemList[i];
 
@@ -5118,8 +5120,8 @@ begin
             // Prioritised position of system-databases
             for i := specialDbs.Count - 1 downto 0 do
               Databases.Insert( 0, specialDbs[i] );
-          end else
-            Databases.Append(DatabasesWanted.Text);
+          end else for i:=0 to DatabasesWanted.Count-1 do
+            Databases.Add(DatabasesWanted[i]);
           Mainform.showstatus( IntToStr( Databases.Count ) + ' Databases', 0 );
           ChildCount := Databases.Count;
           // Avoids excessive InitializeKeywordLists() calls.
@@ -5127,7 +5129,8 @@ begin
           SynSQLSyn1.TableNames.Clear;
           // Let synedit know all database names so that they can be highlighted
           // TODO: Is this right?  Adding "<db name>.<table name>" seems to make more sense..
-          SynSQLSyn1.TableNames.Append( Databases.Text );
+          for i := 0 to Databases.Count - 1 do
+            SynSQLSyn1.TableNames.Add(Databases[i]);
           SynSQLSyn1.TableNames.EndUpdate;
         finally
           MainForm.ShowStatus( STATUS_MSG_READY );

source/fieldeditor.pas
@@ -960,9 +960,12 @@ end;
   Add all columns to index
 }
 procedure TFieldEditForm.btnAddAllColumnsToIndexClick(Sender: TObject);
+var
+  i: Integer;
 begin
   listColumnsUsed.Items.AddStrings(listColumnsAvailable.Items);
-  klist[ComboBoxKeys.ItemIndex].Columns.Append(listColumnsAvailable.Items.Text);
+  for i := 0 to listColumnsAvailable.Items.Count - 1 do
+    klist[ComboBoxKeys.ItemIndex].Columns.Add(listColumnsAvailable.Items[i]);
   listColumnsAvailable.Items.Clear;
   klist[ComboBoxKeys.ItemIndex].Modified := true;
   ValidateControls;