Echnaton70/HeidiSQL

commit ad979ec552e9ec48d8eeb6a314ef4b3a007fb2e4

Fix bug #795 : AV when inserting two rows. Could be more pretty as the second arrow_down unfocusses the grid somehow and no new row is appended. Anyway, cosmetic now.
git-svn-id: http://heidisql.googlecode.com/svn/trunk@1871 560a3889-bb33-0410-9f33-43a8a102dc09

source/childwin.pas
@@ -6024,9 +6024,11 @@ procedure TMDIChild.DataGridInsertRow;
 var
   i, j: Integer;
 begin
-  // Scroll to the bottom to ensure we append the new row at the very last FDataGridResult chunk 
+  // Scroll to the bottom to ensure we append the new row at the very last FDataGridResult chunk
   DataGrid.FocusedNode := DataGrid.GetLast;
   DataGrid.Repaint;
+  DataGrid.BeginUpdate;
+  DataGrid.FocusedNode := DataGrid.AddChild(nil);
   i := Length(FDataGridResult.Rows);
   SetLength(FDataGridResult.Rows, i+1);
   SetLength(FDataGridResult.Rows[i].Cells, Length(FDataGridResult.Columns));
@@ -6034,7 +6036,7 @@ begin
   for j := 0 to Length(FDataGridResult.Rows[i].Cells) - 1 do begin
     FDataGridResult.Rows[i].Cells[j].Text := '';
   end;
-  DataGrid.FocusedNode := DataGrid.AddChild(nil);
+  DataGrid.EndUpdate;
   DataGrid.ClearSelection;
   DataGrid.Selected[DataGrid.FocusedNode] := True;
   DataGridHasChanges := True;

commit 28c2337e95456451175e03067d0d499b79cdf949

Fix unfocused grid on the second arrow_down, broken in r1871
git-svn-id: http://heidisql.googlecode.com/svn/trunk@1872 560a3889-bb33-0410-9f33-43a8a102dc09

source/childwin.pas
@@ -5836,7 +5836,7 @@ procedure TMDIChild.DataGridFocusChanging(Sender: TBaseVirtualTree; OldNode,
     Boolean);
 begin
   // Detect changed focus and update row
-  if Assigned(OldNode) and Assigned(NewNode) and (OldNode <> NewNode) then
+  if Assigned(OldNode) and (OldNode <> NewNode) then
     Allowed := DataGridPostUpdateOrInsert(OldNode)
   else
     Allowed := True;
@@ -6027,8 +6027,8 @@ begin
   // Scroll to the bottom to ensure we append the new row at the very last FDataGridResult chunk
   DataGrid.FocusedNode := DataGrid.GetLast;
   DataGrid.Repaint;
-  DataGrid.BeginUpdate;
-  DataGrid.FocusedNode := DataGrid.AddChild(nil);
+  // Steeling focus now to invoke posting a pending row update
+  DataGrid.FocusedNode := nil;
   i := Length(FDataGridResult.Rows);
   SetLength(FDataGridResult.Rows, i+1);
   SetLength(FDataGridResult.Rows[i].Cells, Length(FDataGridResult.Columns));
@@ -6036,7 +6036,7 @@ begin
   for j := 0 to Length(FDataGridResult.Rows[i].Cells) - 1 do begin
     FDataGridResult.Rows[i].Cells[j].Text := '';
   end;
-  DataGrid.EndUpdate;
+  DataGrid.FocusedNode := DataGrid.AddChild(nil);
   DataGrid.ClearSelection;
   DataGrid.Selected[DataGrid.FocusedNode] := True;
   DataGridHasChanges := True;