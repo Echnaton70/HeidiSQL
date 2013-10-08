Echnaton70/HeidiSQL

commit 374ac8c9eb7a65ee4ecb24a6f0cf4f0fbfbdcf27

Attempt to fix issue #2640 again (AV on alt-tab when in popup editor), by disabling the completion form in ApplicationDeActivate event. Activate MainFormOnTaskBar again, which fixes issue #2351 (Windows Flip3D feature) and probably also issue #3342 (Alt-tab in Wine)
git-svn-id: http://heidisql.googlecode.com/svn/trunk@4530 560a3889-bb33-0410-9f33-43a8a102dc09

source/main.pas
@@ -909,6 +909,7 @@ type
     procedure PopupQueryLoadPopup(Sender: TObject);
     procedure imgDonateClick(Sender: TObject);
     procedure DBtreeExpanded(Sender: TBaseVirtualTree; Node: PVirtualNode);
+    procedure ApplicationDeActivate(Sender: TObject);
   private
     // Executable file details
     FAppVerMajor: Integer;
@@ -11207,6 +11208,15 @@ procedure TMainForm.ApplicationEvents1Deactivate(Sender: TObject);
 begin
   // Force result tab balloon hint to disappear. Does not do so when mouse was moved too fast.
   tabsetQueryMouseLeave(Sender);
+end;
+
+
+procedure TMainForm.ApplicationDeActivate(Sender: TObject);
+begin
+  // Prevent completion window from showing up after Alt-Tab. See issue #2640
+  // and issue #3342
+  // Does not work for some reason in TApplicationEvents.OnDeactivate
+  SynCompletionProposal.Form.Enabled := False;
 end;
 
 

packages/delphiXE/heidisql.dpr
@@ -66,11 +66,13 @@ begin
     AppLanguage := AppSettings.ReadString(asAppLanguage);
     UseLanguage(AppLanguage);
     Application.Initialize;
+    Application.MainFormOnTaskBar := True;
     Application.Title := APPNAME;
     Application.UpdateFormatSettings := False;
     Application.CreateForm(TMainForm, MainForm);
     Application.OnMessage := Mainform.OnMessageHandler;
     MainForm.AfterFormCreate;
+    Application.OnDeactivate := MainForm.ApplicationDeActivate;
     Application.Run;
   end;
  end.

commit 05a0954d6ae684988680f22576ed0f20744afc14

Make completion proposal usable for mouse users again, after mainform has focus again. Broken in r4530. See issue #2640.
git-svn-id: http://heidisql.googlecode.com/svn/trunk@4555 560a3889-bb33-0410-9f33-43a8a102dc09

source/main.pas
@@ -5269,6 +5269,9 @@ begin
   if not CanExecute then
     Exit;
 
+  // Work around for issue #2640. See ApplicationDeActivate
+  Proposal.Form.Enabled := True;
+
   rx := TRegExpr.Create;
 
   // Find token1.token2.token3, while cursor is somewhere in token3
@@ -11214,6 +11217,7 @@ begin
   // and issue #3342
   // Does not work for some reason in TApplicationEvents.OnDeactivate
   SynCompletionProposal.Form.Enabled := False;
+  // Gets activated again in SynCompletionProposalExecute
 end;
 
 